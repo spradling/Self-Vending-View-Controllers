8e8db472a9aec64952ca11c62293feb8e83fa949
Chris Spradling <chris.spradling@mobileforming.com>
2019-02-14 10:47:15 -0800

    file structure cleanup, added an R&D section

---
+++ Self-Vending ViewControllers/TheFuture/SelfVendingCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  SelfVendingCollectionViewCell.swift
+//  Self-Vending ViewControllers
+//
+//  Created by Christopher Spradling on 2/13/19.
+//  Copyright © 2019 cspantech. All rights reserved.
+//
+
+import UIKit
+
+
+protocol SelfVendingCollectionViewCell: SelfVendingView {
+    
+    static var identifier: String { get }
+    
+}
+
+extension SelfVendingCollectionViewCell where Self: UICollectionViewCell {
+    
+    static var identifier: String { return String(describing: Self.self) }
+    
+    static func cell(for collectionView: UICollectionView, indexPath: IndexPath) -> Self? {
+        
+        return collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? Self
+    }
+    
+    static func register(to collectionView: UICollectionView) {
+        collectionView.register(nibName?.nib, forCellWithReuseIdentifier: identifier)
+    }
+    
+    
+    
+}
